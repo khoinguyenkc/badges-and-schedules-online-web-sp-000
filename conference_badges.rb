f2dfc8fcb3dd4da4b40ba55c640486c8be4b860f
Vietkhoi Nguyen <khoinguyenkc@gmail.com>
2020-08-14 01:07:40 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,3 +4,10 @@ def badge_maker(name)
   "Hello, my name is #{name}"
 end
 puts badge_maker("beyonce")
+
+
+def batch_badge_creator(arrayofnames)
+  arrayofnames.each do |name|
+    "Hello, my name is #{name}"
+  end
+end
